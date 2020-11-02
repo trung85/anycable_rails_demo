8dc08399ebba4be4a05155548342581209114319
Tade <phan.minh.trung@moneyforward.vn>
2020-11-02 15:17:11 +0700

    fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_anycable_custom_session', domain: :all
+Rails.application.config.session_store :cookie_store, key: '_anycable_custom_session', domain: :all, secure: !(Rails.env.development? || Rails.env.test?)
